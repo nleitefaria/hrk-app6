17f048facfafc32f9b22710709da06e9ead12e4f
nleitefaria <nleitefaria@nleitefaria-Latitude-5490>
2019-12-10 14:47:01 +0000

    Added SORT

--- src/main/java/com/mycompany/hkrapp6/service/impl/OrdersServiceImpl.java
+++ src/main/java/com/mycompany/hkrapp6/service/impl/OrdersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mycompany.hkrapp6.entity.Orders;
@@ -29,7 +30,7 @@ public class OrdersServiceImpl implements OrdersService {
 	
 	public Page<Orders> findAllPaged(int page) 
     {
-        return repository.findAll(PageRequest.of(page, 10));
+        return repository.findAll(PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC,"id")));
     }
 
 }
